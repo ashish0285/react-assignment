ac0c7080547cddb1deeabbc55849b5779353cd9d
Ashish Mishra <ashish.mishra@yahoo.co.in>
2022-10-15 10:15:40 +0530

    UserPeofile commits

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import {Header} from './components/Header';
+import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Profile from './components/Profile';
+import ProfileForm from './components/ProfileForm';
 
 function App() {
   return (
-    <div className="container">
-      <Header title="Hello"/>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route >
+            <Route exec path="/" element={<ProfileForm />} />
+            <Route exec path="Profile" element={<Profile />} />
+          </Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
+
   );
 }
 
