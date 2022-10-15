9fcdff1fe4f74952f3dbf1a5fbff2126753798f9
Ashish Mishra <ashish.mishra@yahoo.co.in>
2022-10-15 11:06:36 +0530

    todo App Completed

--- src/index.js
+++ src/index.js
@@ -2,16 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const tasks = [
+  { id: "todo:0", name: "Coding", completed: true },
+  { id: "todo:1", name: "Meeting", completed: false },
+  { id: "todo:2", name: "Sleep", completed: false }
+];
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App taskList={tasks}/>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
